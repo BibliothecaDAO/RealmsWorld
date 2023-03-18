712fcb41b1131b1894f21d1e464658ae0d1961d8
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-03-18 21:22:52 +1100

    fix api

--- src/app/collection/[address]/page.tsx
+++ src/app/collection/[address]/page.tsx
@@ -11,19 +11,14 @@ export default async function Page({
 }) {
   const collection = await getData({ id: params.address }, "collection");
 
-
-  console.log(collection)
-
   const attributes = await getData(
     { collection: params.address },
     "attributes"
   );
 
   return (
-    // <div className="h-full">
-    //   <div className="object-cover w-full bg-no-repeat h-96 bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1),rgba(32,32,32,1)),url('/dummy_background.png')]" />
-      <div className="flex h-full p-8 -mt-64">
-        {/* <div className="flex-none w-64 pt-8 rounded-t-full bg-gradient-to-b from-theme-gray-light">
+    <div className="flex h-full p-8 -mt-64">
+      <div className="flex-none w-64 pt-8 rounded-t-full bg-gradient-to-b from-theme-gray-light">
         <Image
           src={collection.collections[0].image} // Use the path to your image
           alt="An example image" // Provide a descriptive alt text
@@ -32,16 +27,14 @@ export default async function Page({
           className="mx-auto border rounded-full"
         />
         <Attributes address={params.address} attributes={attributes} />
-
-        </div>
-        <div className="flex-grow p-8">
-          <h1>{collection.collections[0].name}</h1>{" "}
-          <TokenTable
-            address={params.address}
-            collection={collection.collections[0]}
-          />
-        </div> */}
       </div>
-    // </div>
+      <div className="flex-grow p-8">
+        <h1>{collection.collections[0].name}</h1>{" "}
+        <TokenTable
+          address={params.address}
+          collection={collection.collections[0]}
+        />
+      </div>
+    </div>
   );
 }
