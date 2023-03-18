8c18f23b5f4f74f8db860a7043cc25ae49e31dff
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-03-18 21:17:04 +1100

    tesT

--- src/app/collection/[address]/page.tsx
+++ src/app/collection/[address]/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({
   const collection = await getData({ id: params.address }, "collection");
 
 
-  console.log(collection.collections[0])
+  console.log(collection)
 
   const attributes = await getData(
     { collection: params.address },
@@ -23,7 +23,7 @@ export default async function Page({
     // <div className="h-full">
     //   <div className="object-cover w-full bg-no-repeat h-96 bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1),rgba(32,32,32,1)),url('/dummy_background.png')]" />
       <div className="flex h-full p-8 -mt-64">
-        <div className="flex-none w-64 pt-8 rounded-t-full bg-gradient-to-b from-theme-gray-light">
+        {/* <div className="flex-none w-64 pt-8 rounded-t-full bg-gradient-to-b from-theme-gray-light">
         <Image
           src={collection.collections[0].image} // Use the path to your image
           alt="An example image" // Provide a descriptive alt text
@@ -40,7 +40,7 @@ export default async function Page({
             address={params.address}
             collection={collection.collections[0]}
           />
-        </div>
+        </div> */}
       </div>
     // </div>
   );
