0e73b18fc475497cafc7e8029883b3f576801558
TheSkyvalkeR <theskyvalker10@gmail.com>
2023-05-12 00:39:16 +0800

    minor changes to activity timestamp display

--- src/app/components/ActivityCard.tsx
+++ src/app/components/ActivityCard.tsx
@@ -11,24 +11,27 @@ export const ActivityCard = ({ activity }: ActivityCardProps) => {
   // convert unix to time
   const date = new Date(activity.timestamp * 1000);
 
-  const getElapsedTime = () => {
+  function getElapsedTime() {
     // get time difference from now
     const timeDiff = Math.abs(Date.now() - date.getTime());
 
-    // get time difference in hours
-    const hours = Math.floor(timeDiff / 1000 / 60 / 60);
-
-    // get the difference in other units to change the display
+    // get time difference in various units
+    const seconds = Math.floor(timeDiff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
     const weeks = Math.floor(days / 7);
     const months = Math.floor(days / 30);
     const years = Math.floor(days / 365);
 
-    if (hours < 48) return `${hours} hours ago`;
-    if (days < 7) return `${days} days ago`;
-    if (weeks < 4) return `${weeks} weeks ago`;
-    if (months < 12) return `${months} months ago`;
-    return `${years} years ago`;
+    // return the most appropriate unit
+    if (seconds < 60) return `${seconds} second${seconds === 1 ? '' : 's'} ago`;
+    if (minutes < 60) return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
+    if (hours < 24) return `${hours} hour${hours === 1 ? '' : 's'} ago`;
+    if (days < 7) return `${days} day${days === 1 ? '' : 's'} ago`;
+    if (weeks < 4) return `${weeks} week${weeks === 1 ? '' : 's'} ago`;
+    if (months < 12) return `${months} month${months === 1 ? '' : 's'} ago`;
+    return `${years} year${years === 1 ? '' : 's'} ago`;
   };
 
   const getLocalizedDate = () => {
