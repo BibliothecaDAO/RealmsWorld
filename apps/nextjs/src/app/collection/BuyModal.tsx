bb5147eb111b7eefd45111c16a3a1f66797dfc02
red <redbeard.eth@gmail.com>
2023-10-13 17:20:33 +1000

    fix buy modal

--- apps/nextjs/src/app/collection/BuyModal.tsx
+++ apps/nextjs/src/app/collection/BuyModal.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from "@/app/_components/ui/button";
 import { BuyModal } from "@reservoir0x/reservoir-kit-ui";
+import { useModal } from "connectkit";
 
 export const BuyButton = ({ address, id, size = "sm" }: any) => {
+  const { setOpen } = useModal();
   return (
     <BuyModal
       trigger={
@@ -17,7 +19,7 @@ export const BuyButton = ({ address, id, size = "sm" }: any) => {
       onPurchaseError={(error, data) =>
         console.log("Transaction Error", error, data)
       }
-      onConnectWallet={() => console.log("add wallet connector")}
+      onConnectWallet={() => setOpen}
       onClose={(data, stepData, currentStep) => console.log("Modal Closed")}
     />
   );
