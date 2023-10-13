ed3783ada206de4bdd9740845b567c173bb8e1d2
red <redbeard.eth@gmail.com>
2023-10-13 16:54:48 +1000

    fix inngest build

--- apps/nextjs/src/app/api/inngest/route.ts
+++ apps/nextjs/src/app/api/inngest/route.ts
@@ -3,4 +3,10 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { fetchMetadata } from "../../../inngest/fetch_metadata"; // Your own functions
 
-export const { GET, POST, PUT } = serve(inngest, [fetchMetadata]);
+export const runtime = "edge";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [fetchMetadata],
+  streaming: "allow",
+});
