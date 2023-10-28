87e7f8795e9f7dfd5c8ec8ebabe158ae38a4f93c
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-10-28 12:38:58 +1100

    udpate react query, auth

--- apps/nextjs/src/app/collection/page.tsx
+++ apps/nextjs/src/app/collection/page.tsx
@@ -15,7 +15,6 @@ export default async function Page() {
   const l1Collections = await getCollections([
     { contract: getTokenContractAddresses("realms").L1! },
   ]);
-  console.log(getTokenContractAddresses("realms").L1);
   // TODO refine collection display logic (with l2 collections included)
   const collections: Collection[] = l1Collections?.collections;
   const defaultImage = "/backgrounds/map.png";
