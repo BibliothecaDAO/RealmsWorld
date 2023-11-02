1faf64c6b48fecd44b632dfc04a032b78f291d95
RedBeard <90423049+RedBeardEth@users.noreply.github.com>
2023-11-03 07:01:11 +1100

    updat metadata collection

--- apps/nextjs/src/app/collection/[id]/(list)/page.tsx
+++ apps/nextjs/src/app/collection/[id]/(list)/page.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import type { erc721Tokens } from "@/constants";
+import { erc721Tokens } from "@/constants";
 import { getAttributes } from "@/lib/reservoir/getAttributes";
 import { getCollections } from "@/lib/reservoir/getCollections";
 import { getToken } from "@/lib/reservoir/getToken";
 import type { Collection } from "@/types";
-import { getTokenContractAddresses, isStarknetAddress } from "@/utils/utils";
+import { getTokenContractAddresses } from "@/utils/utils";
 
 import Mint from "./Mint";
 import { Trade } from "./Trade";
@@ -16,14 +16,14 @@ export async function generateMetadata({
 }: {
   params: { id: string };
 }): Promise<Metadata> {
-  const tokenAddresses = getTokenContractAddresses(
+  /*const tokenAddresses = getTokenContractAddresses(
     params.id as keyof typeof erc721Tokens,
   );
   const collectionData = await getCollections([
     { contract: tokenAddresses.L1 ?? params.id },
   ]);
-  const collection: Collection = collectionData.collections?.[0];
-
+  const collection: Collection = collectionData.collections?.[0];*/
+  const collection = erc721Tokens[params.id as keyof typeof erc721Tokens];
   return {
     title: `Collection: ${collection?.name}`,
     description: `Collection Details and Marketplace for ${collection?.name} - Created for adventurers by Bibliotheca DAO`,
