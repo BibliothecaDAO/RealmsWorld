9987f86eb0eb335f5af92b71857da9bb648ae6ce
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-11-03 17:09:36 +1100

    to revert - remove edge functions

--- apps/nextjs/src/app/api/inngest/route.ts
+++ apps/nextjs/src/app/api/inngest/route.ts
@@ -2,7 +2,7 @@ import { inngest } from "@/inngest/client";
 import { fetchMetadata } from "@/inngest/fetch_metadata"; // Your own functions
 import { serve } from "inngest/next";
 
-export const runtime = "edge";
+//export const runtime = "edge";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
