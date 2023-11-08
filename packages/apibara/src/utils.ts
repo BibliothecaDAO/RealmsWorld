dbfb2084dcba516b0660098d9654347ebe08f9f6
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-11-09 09:47:02 +1100

    test indexers

--- packages/apibara/src/utils.ts
+++ packages/apibara/src/utils.ts
@@ -9,5 +9,7 @@ export const erc721ContractEvents = whitelistedContracts.map((contract) => {
   return {
     fromAddress: contract,
     keys: [hash.getSelectorFromName("Transfer") as `0x${string}`],
+    includeTransaction: false, // Don't send transaction data.
+    includeReceipt: false, // Don't send receipt data.
   };
 });
