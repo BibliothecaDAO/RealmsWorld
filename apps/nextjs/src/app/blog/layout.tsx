3cada73168b1365a30c8eccc23a3c6d960487fd6
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-11-09 15:28:35 +1100

    responsive text

--- apps/nextjs/src/app/blog/layout.tsx
+++ apps/nextjs/src/app/blog/layout.tsx
@@ -1,8 +1,10 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="bg-dark-green mx-auto mt-12 max-w-[80ch] space-y-6 border px-10 py-6 leading-relaxed [&>*]:list-inside [&>*]:list-disc">
-      {children}
+    <div className="max-w-screen">
+      <div className="bg-dark-green mx-auto mt-12 max-w-[80ch] space-y-6 break-words border px-10 py-6 leading-relaxed [&>*]:list-inside [&>*]:list-disc">
+        {children}
+      </div>
     </div>
   );
 }
