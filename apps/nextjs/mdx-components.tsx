8912f91f6855fad8ceb64a1f71ad93ff548ddf08
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-11-09 20:27:16 +1100

    add to homepage and some content fixes

--- apps/nextjs/mdx-components.tsx
+++ apps/nextjs/mdx-components.tsx
@@ -3,7 +3,14 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    img: (props) => <Image width={1600} height={900} {...props} />,
+    img: (props) => (
+      <Image
+        width={1600}
+        height={900}
+        className="max-w-screen my-8"
+        {...props}
+      />
+    ),
     ...components,
   };
 }
--- apps/nextjs/src/app/blog/layout.tsx
+++ apps/nextjs/src/app/blog/layout.tsx
@@ -1,8 +1,8 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="max-w-screen">
-      <div className="bg-dark-green mx-auto mt-12 max-w-[80ch] space-y-6 break-words border px-10 py-6 leading-relaxed [&>*]:list-inside [&>*]:list-disc">
+    <div className="mx-auto max-w-4xl">
+      <div className="bg-dark-green mx-2 mt-20 space-y-6 overflow-y-auto text-ellipsis border px-6 py-6 leading-relaxed sm:px-10 [&>*]:list-inside [&>*]:list-disc">
         {children}
       </div>
     </div>
