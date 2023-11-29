4a5639862a83ed6d7c1d99bf7e0819808753d3fe
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-11-29 11:40:56 +1100

    test

--- packages/apibara/src/erc721_tokens.ts
+++ packages/apibara/src/erc721_tokens.ts
@@ -12,7 +12,7 @@ import { uint256 } from "https://esm.sh/starknet";
 //import { erc721ContractEvents } from "./utils.ts";
 
 export const config: Config<Starknet, Postgres> = {
-  //streamUrl: Deno.env.get("STREAM_URL"),
+  streamUrl: Deno.env.get("STREAM_URL"),
   //startingBlock: Number(Deno.env.get("ERC721_STARTING_BLOCK")),
   network: "starknet",
   batchSize: 1,
