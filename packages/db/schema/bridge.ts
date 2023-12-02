7d510b28e3bca8510b3e1f275ba1673f93a93fa4
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2023-12-02 21:00:02 +1100

    update schema for bridge and lockfile

--- packages/db/schema/bridge.ts
+++ packages/db/schema/bridge.ts
@@ -1,9 +1,9 @@
-import { integer, numeric, text, timestamp } from "drizzle-orm/pg-core";
+import { bigint, numeric, text, timestamp } from "drizzle-orm/pg-core";
 
 import { pgSqlTable } from "./_table";
 
 export const bridge = pgSqlTable("bridge", {
-  _cursor: integer("_cursor"),
+  _cursor: bigint("_cursor", { mode: "number" }),
   hash: text("hash").primaryKey(),
   type: text("type"),
   l1Account: text("l1Account"),
