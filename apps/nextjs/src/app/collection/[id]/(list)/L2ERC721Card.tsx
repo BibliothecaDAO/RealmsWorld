58c173604b98c29b97e9090fc715360ba8212d7a
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-12-12 17:56:35 +1100

    cleanup

--- apps/nextjs/src/app/collection/[id]/(list)/L2ERC721Card.tsx
+++ apps/nextjs/src/app/collection/[id]/(list)/L2ERC721Card.tsx
@@ -4,15 +4,82 @@ import { useStarkDisplayName } from "@/hooks/useStarkName";
 import type { RouterOutputs } from "@/utils/api";
 import { findTokenName } from "@/utils/utils";
 
-interface TokenCardProps {
+export const L2ERC721Card = ({
+  token,
+  layout = "grid",
+}: {
   token: RouterOutputs["erc721Tokens"]["all"]["items"][number];
   layout?: "grid" | "list";
-}
+}) => {
+  const isGrid = layout === "grid";
+  const imageSize = isGrid ? 800 : 80;
 
-const TokenAttributes = ({ token, attributeKeys }: any) => (
+  return (
+    <div
+      className={`transform border-2 bg-dark-green duration-300 hover:border-white ${
+        isGrid ?? "flex w-full"
+      }`}
+    >
+      <Link
+        className={isGrid ? "" : "flex w-full"}
+        href={`/collection/${findTokenName(token.contract_address ?? "")}/${
+          token.token_id
+        }`}
+      >
+        <div className={` ${!isGrid && "p-2"} relative`}>
+          {token.image && (
+            <Image
+              src={token.image}
+              alt={token.name ?? `beasts-${token.token_id}`}
+              className={isGrid ? "mx-auto" : ""}
+              width={imageSize}
+              height={imageSize}
+            />
+          )}
+          {isGrid && (
+            <span className="absolute bottom-3 right-3 border bg-black px-3 py-1 text-xs">
+              #{token.token_id}
+            </span>
+          )}
+        </div>
+
+        <TokenDetails
+          token={token}
+          isGrid={isGrid}
+          starkName={useStarkDisplayName(
+            token.transfers[0]?.toAddress ?? token.minter ?? "",
+          )}
+        />
+      </Link>
+    </div>
+  );
+};
+
+const TokenDetails = ({
+  token,
+  isGrid,
+  starkName,
+}: {
+  token: RouterOutputs["erc721Tokens"]["all"]["items"][number];
+  isGrid: boolean;
+  starkName?: string;
+}) =>
+  isGrid ? (
+    <GridDetails token={token} starkName={starkName} />
+  ) : (
+    <ListDetails token={token} starkName={starkName} />
+  );
+
+const TokenAttributes = ({
+  token,
+  attributeKeys,
+}: {
+  token: RouterOutputs["erc721Tokens"]["all"]["items"][number];
+  attributeKeys: string[];
+}) => (
   <table className="min-w-full text-xs">
     <tbody>
-      {attributeKeys.map((key: any) => {
+      {attributeKeys.map((key: string) => {
         const attribute = token.metadata?.attributes?.find(
           (trait: any) => trait.trait_type === key,
         );
@@ -27,68 +94,22 @@ const TokenAttributes = ({ token, attributeKeys }: any) => (
   </table>
 );
 
-export const L2ERC721Card = ({ token, layout = "grid" }: TokenCardProps) => {
-  const isGrid = layout === "grid";
-  const imageSize = isGrid ? 800 : 80;
-  const tokenOwner = token.transfers[0]?.toAddress ?? token.minter;
-  const starkName = useStarkDisplayName(tokenOwner || "");
-
-  const cardClassName = isGrid
-    ? "bg-dark-green duration-300 transform border border-2 hover:border-white"
-    : "duration-300 transform bg-dark-green flex w-full border-2  hover:border-white";
-
-  const imageAlt = token.name || `beasts-${token.token_id}`;
-
-  return (
-    <div className={cardClassName}>
-      <Link
-        className={isGrid ? "" : "flex w-full"}
-        href={`/collection/${findTokenName(token.contract_address ?? "")}/${
-          token.token_id
-        }`}
-      >
-        <div className={` ${!isGrid && "p-2"} relative`}>
-          {token.image && (
-            <Image
-              src={token.image}
-              alt={imageAlt}
-              className={isGrid ? "mx-auto" : ""}
-              width={imageSize}
-              height={imageSize}
-            />
-          )}
-          {isGrid && (
-            <span className="absolute bottom-3 right-3 border bg-black px-3 py-1 text-xs">
-              #{token.token_id}
-            </span>
-          )}
-        </div>
-
-        <TokenDetails token={token} isGrid={isGrid} starkName={starkName} />
-      </Link>
-    </div>
-  );
-};
-
-const TokenDetails = ({ token, isGrid, starkName }: any) =>
-  isGrid ? (
-    <GridDetails token={token} starkName={starkName} isGrid />
-  ) : (
-    <ListDetails token={token} starkName={starkName} />
-  );
-
-const GridDetails = ({ token, starkName }: any) => (
+const GridDetails = ({
+  token,
+  starkName,
+}: {
+  token: RouterOutputs["erc721Tokens"]["all"]["items"][number];
+  starkName?: string;
+}) => (
   <div className="w-full p-3">
     <div className="flex justify-between border-b pb-2">
-      <span className="">{decodeURIComponent(token.name || "")}</span>
+      <span className="">{decodeURIComponent(token.name ?? "")}</span>
     </div>
-
     <TokenAttributes
       token={token}
       attributeKeys={["type", "tier", "level", "health"]}
     />
     <Price />
-    {/* <ViewButton token={token} isGrid /> */}
     <div className="mt-3 text-xs opacity-70">{starkName}</div>
   </div>
 );
@@ -108,11 +129,17 @@ const Price = () => {
   );
 };
 
-const ListDetails = ({ token, starkName }: any) => {
+const ListDetails = ({
+  token,
+  starkName,
+}: {
+  token: RouterOutputs["erc721Tokens"]["all"]["items"][number];
+  starkName?: string;
+}) => {
   return (
     <div className="px-6 py-3">
       <div className="flex justify-between border-b pb-2">
-        <span className="">{decodeURIComponent(token.name || "")}</span>
+        <span className="">{decodeURIComponent(token.name ?? "")}</span>
       </div>
       <div className="mt-3 text-xs opacity-70">{starkName}</div>
     </div>
