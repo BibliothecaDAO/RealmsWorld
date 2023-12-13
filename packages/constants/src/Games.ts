65b839f6eaba73ce7fb1453c2bc1c2431c520688
Willem Olding <willemolding@gmail.com>
2023-12-13 13:25:06 +1100

    add icon and change description to name

--- packages/constants/src/Games.ts
+++ packages/constants/src/Games.ts
@@ -226,7 +226,7 @@ export const games: Game[] = [
     genres: ["Play to Die", "Card Game"],
     color: "#f5f5f5",
     status: "development",
-    description: "Balance Power and Chaos",
+    description: "Arcane Assembler",
     longform:
       "Arcane Assembly is a Play2Die card game where you must build the most powerful spell you can... without exploding first. Forage for ingredients, perform rituals, and summon helpers but manage your time wisely. Spells left unattended quickly become unstable. Compete with other mages from across the realm for bragging rights and rewards.",
     links: {
