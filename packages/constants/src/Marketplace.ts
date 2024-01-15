575d62db52100e2053c30bb82241f284d5e3c548
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-01-16 08:55:31 +1100

    update apibara mainnet env

--- packages/constants/src/Marketplace.ts
+++ packages/constants/src/Marketplace.ts
@@ -7,7 +7,8 @@ export const MarketplaceCollectionIds = {
 };
 
 export const MarketplaceContract: Record<number | string, string> = {
-  [ChainId.SN_MAIN]: "",
+  [ChainId.SN_MAIN]:
+    "0x07bb44fd93f79c3529acc8b03fdd749f60e8ed4e0e57364eef6a971550cccae4",
   [ChainId.SN_SEPOLIA]:
     "0x0297e088cd7777bebda7024e2dde81e9b745f41e5de0589c91de5caa885d9c32",
 };
