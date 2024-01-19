8a19ec42b3bfacfbdca4d4bffc97f9e55554ec09
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-01-19 19:02:23 +1100

    conditional

--- apps/nextjs/src/app/_components/staking/PaymentPoolV2.tsx
+++ apps/nextjs/src/app/_components/staking/PaymentPoolV2.tsx
@@ -9,18 +9,22 @@ export const PaymentPoolV2 = () => {
 
   return (
     <div className="flex-col border p-8 text-lg">
-      <div>
-        <div className="mb-5 flex justify-center space-x-2 text-2xl ">
-          {numTokens ? numTokens.toLocaleString() : 0}{" "}
-          <Lords className="ml-2 h-5 w-5 self-center fill-current" />
-        </div>
+      {numTokens > 0 ? (
+        <div>
+          <div className="mb-5 flex justify-center space-x-2 text-2xl ">
+            {numTokens ? numTokens.toLocaleString() : 0}{" "}
+            <Lords className="ml-2 h-5 w-5 self-center fill-current" />
+          </div>
 
-        {numTokens && (
-          <Button onClick={claimAirdrop}>
-            {alreadyClaimed ? "claimed" : "claim lords"}
-          </Button>
-        )}
-      </div>
+          {numTokens && (
+            <Button onClick={claimAirdrop}>
+              {alreadyClaimed ? "claimed" : "claim lords"}
+            </Button>
+          )}
+        </div>
+      ) : (
+        "No tokens to claim"
+      )}
     </div>
   );
 };
