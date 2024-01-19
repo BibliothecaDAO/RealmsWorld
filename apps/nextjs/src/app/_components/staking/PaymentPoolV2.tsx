3cd6f60c14b9bf54374cc0591792bb10903c5247
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-01-19 21:19:07 +1100

    added payment pool mainnet

--- apps/nextjs/src/app/_components/staking/PaymentPoolV2.tsx
+++ apps/nextjs/src/app/_components/staking/PaymentPoolV2.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useAirdropClaim } from "@/hooks/useAirdropClaim";
 import Lords from "@/icons/lords.svg";
 
@@ -7,8 +6,6 @@ import { Button } from "@realms-world/ui";
 export const PaymentPoolV2 = () => {
   const { numTokens, alreadyClaimed, claimAirdrop, balance } =
     useAirdropClaim();
-
-  console.log("balance", balance);
 
   return (
     <div className="flex-col border p-8 text-lg">
