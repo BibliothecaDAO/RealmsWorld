98f24597a198d5d21f876f2a00192bb0bccff56b
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-02-22 22:57:55 +1100

    add blobert to account page, collection list style

---
+++ apps/nextjs/src/app/user/[address]/blobert/page.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from "react";
+import { LoadingSkeletonGrid } from "@/app/_components/LoadingSkeletonGrid";
+import L2ERC721Table from "@/app/collection/[id]/(list)/L2ERC721Table";
+import { SUPPORTED_L2_CHAIN_ID } from "@/constants/env";
+
+import { Collections, getCollectionAddresses } from "@realms-world/constants";
+
+export default async function Page({
+  params,
+}: {
+  params: { address: string };
+}) {
+  return (
+    <Suspense fallback={<LoadingSkeletonGrid />}>
+      <L2ERC721Table
+        contractAddress={
+          getCollectionAddresses(Collections.BLOBERT)[SUPPORTED_L2_CHAIN_ID]!
+        }
+        ownerAddress={params.address}
+      />
+    </Suspense>
+  );
+}
