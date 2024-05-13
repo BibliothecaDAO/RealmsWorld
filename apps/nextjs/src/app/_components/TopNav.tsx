b56e53ca68a1b7854c0566a36ae4c46ca8be3281
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-05-13 11:15:09 +1000

    fix lint

--- apps/nextjs/src/app/_components/TopNav.tsx
+++ apps/nextjs/src/app/_components/TopNav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Bookmark from "@/icons/bookmark.svg";
 import PieChart from "@/icons/pie-chart.svg";
 import RealmsL3 from "@/icons/realms_l3.svg";
-import { HammerIcon, Menu, ShieldQuestion } from "lucide-react";
+import { HammerIcon, Menu /*, ShieldQuestion*/ } from "lucide-react";
 
 import {
   Button,
