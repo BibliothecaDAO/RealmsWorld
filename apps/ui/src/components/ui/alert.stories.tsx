8583fe59ef3c697226b7ca0ed2c6855f226de024
zooniebin <transparent1123@gmail.com>
2024-06-07 16:47:35 -0400

    add alert stories and accordion stories

---
+++ apps/ui/src/components/ui/alert.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Alert } from "./alert";
+
+const meta: Meta<typeof Alert> = {
+  title: "ui/Alert",
+  // @ts-expect-error
+  component: Alert,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+export default meta;
+
+type Story = StoryObj<typeof Alert>;
+
+export const Base: Story = {
+  render: (args) => <Alert {...args} />,
+  args: {
+    title: "Information",
+    message: "This is an info alert",
+    variant: "info",
+  },
+};
+
+export const Success: Story = {
+  render: (args) => <Alert {...args} />,
+  args: {
+    title: "",
+    message: "This is a success alert",
+    variant: "success",
+  },
+};
+
+export const Warning: Story = {
+  render: (args) => <Alert {...args} />,
+  args: {
+    title: "",
+    message: "This is a warning alert",
+    variant: "warning",
+  },
+};
+
+export const Error: Story = {
+  render: (args) => <Alert {...args} />,
+  args: {
+    title: "",
+    message: "This is an error alert",
+    variant: "error",
+  },
+};
