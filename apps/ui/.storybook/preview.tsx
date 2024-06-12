7cac345b38e5b2795e5fc3706ab4911adc801f5c
zooniebin <transparent1123@gmail.com>
2024-06-12 13:57:45 -0400

    fix fonts

--- apps/ui/.storybook/preview.tsx
+++ apps/ui/.storybook/preview.tsx
@@ -1,10 +1,9 @@
 import type { Preview } from "@storybook/react";
-import { Inconsolata, Silkscreen } from "next/font/google";
-
-import "./tailwind-imports.css";
-import "@realms-world/styles/globals.css";
+//import "./tailwind-imports.css";
+//import "@realms-world/styles/globals.css";
 
 import * as React from "react";
+import { Inconsolata, Silkscreen } from "next/font/google";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 import { ThemeProvider } from "@storybook/theming";
 
@@ -13,23 +12,23 @@ import { Toaster } from "../src/components/ui/toaster";
 
 import "./globals.css";
 
-const baiJamjuree = Silkscreen({
+const silkscreen = Silkscreen({
   subsets: ["latin"],
-  variable: "--font-bai-jamjuree",
+  variable: "--font-silkscreen",
   weight: ["400"],
   display: "swap",
 });
 
-const karla = Inconsolata({
+const inconsolata = Inconsolata({
   subsets: ["latin"],
-  variable: "--font-karla",
+  variable: "--font-inconsolata",
   weight: "400",
   display: "swap",
 });
 
 export const decorators = [
   (Story) => (
-    <div className={`${baiJamjuree.variable} ${karla.variable}`}>
+    <div className={`${silkscreen.variable} ${inconsolata.variable} dark`}>
       <ToastProvider>
         <Story />
         <Toaster />
