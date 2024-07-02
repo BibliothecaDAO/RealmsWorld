c734378b943ab1243c4237edf291792e8629a69d
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-07-02 19:52:56 +1000

    update card text colors and signin process

--- apps/nextjs/src/app/account/profile/Profile.tsx
+++ apps/nextjs/src/app/account/profile/Profile.tsx
@@ -8,11 +8,13 @@ import { padAddress, shortenHex } from "@/utils/utils";
 import { useAccount } from "@starknet-react/core";
 import { shortenAddress } from "@starkware-industries/commons-js-utils";
 import { UserRoundPlus } from "lucide-react";
+import { SignInSIWS } from "./SignInSIWS";
 
 import type { RouterOutputs } from "@realms-world/api";
 import {
   Badge,
   Button,
+  Card,
   CardContent,
   CardHeader,
   CardTitle,
@@ -50,89 +52,96 @@ export const Profile = ({
     },
   );
   return (
-    <>
-      {delegate ? (
-        <>
-          <CardHeader>
-            <CardTitle className="flex gap-2">
-              <Image
-                alt="profile image"
-                width={16}
-                height={16}
-                src="https://avatars.githubusercontent.com/u/1?v=4"
-                className="h-16 w-16 rounded-full"
-              />
-              <div className="flex w-full justify-between">
-                <div>
-                  <div className="mb-1">{shortenAddress(delegate.id)}</div>
-                  <div className="mb-1 flex items-center text-sm font-bold uppercase text-muted-foreground">
-                    Voting Power:
-                    <Badge variant="outline" className="ml-2">
-                      {delegate.delegatedVotesRaw} Realms
-                    </Badge>
+    <div className="grid grid-cols-5 gap-x-6">
+      <Card className="col-span-3">
+        {delegate ? (
+          <>
+            <CardHeader>
+              <CardTitle className="flex gap-2">
+                <Image
+                  alt="profile image"
+                  width={16}
+                  height={16}
+                  src="https://avatars.githubusercontent.com/u/1?v=4"
+                  className="h-16 w-16 rounded-full"
+                />
+                <div className="flex w-full justify-between">
+                  <div>
+                    <div className="mb-1">{shortenAddress(delegate.id)}</div>
+                    <div className="mb-1 flex items-center text-sm font-bold uppercase text-muted-foreground">
+                      Voting Power:
+                      <Badge variant="outline" className="ml-2">
+                        {delegate.delegatedVotesRaw} Realms
+                      </Badge>
+                    </div>
+                    <div className="flex items-center gap-2 text-muted-foreground">
+                      <span className="text-sm font-bold uppercase">
+                        Delegation:
+                      </span>
+                      <Badge variant="outline">
+                        {tokenHolder?.tokenBalanceRaw} Realms
+                      </Badge>
+                      <span className="text-sm">delegated to</span>
+                      <Badge variant="outline">
+                        {tokenHolder?.delegate == delegate.id
+                          ? "self"
+                          : shortenHex(tokenHolder?.delegate ?? "0x", 8)}
+                      </Badge>
+                    </div>
                   </div>
-                  <div className="flex items-center gap-2 text-muted-foreground">
-                    <span className="text-sm font-bold uppercase">
-                      Delegation:
-                    </span>
-                    <Badge variant="outline">
-                      {tokenHolder?.tokenBalanceRaw} Realms
-                    </Badge>
-                    <span className="text-sm">delegated to</span>
-                    <Badge variant="outline">
-                      {tokenHolder?.delegate == delegate.id
-                        ? "self"
-                        : shortenHex(tokenHolder?.delegate ?? "0x", 8)}
-                    </Badge>
-                  </div>
+                  {tokenHolder?.delegate != padAddress(address) && (
+                    <Button size="sm" onClick={() => delegateRealms()}>
+                      <UserRoundPlus className="mr-2" /> Delegate to Self
+                    </Button>
+                  )}
                 </div>
-                {tokenHolder?.delegate != padAddress(address) && (
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+              <ProfileForm
+                delegateProfile={delegate.delegateProfile}
+                delegateId={delegate.id}
+              />
+            </CardContent>
+          </>
+        ) : (
+          <>
+            <CardHeader>
+              <CardTitle>No delegate found</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {tokenHolder?.tokenBalanceRaw ? (
+                <>
+                  Delegate your {tokenHolder.tokenBalanceRaw} Realms to yourself
+                  to create a delegate profile
                   <Button size="sm" onClick={() => delegateRealms()}>
                     <UserRoundPlus className="mr-2" /> Delegate to Self
                   </Button>
-                )}
-              </div>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-4">
-            <ProfileForm
-              delegateProfile={delegate.delegateProfile}
-              delegateId={delegate.id}
-            />
-          </CardContent>
-        </>
-      ) : (
-        <>
-          <CardHeader>
-            <CardTitle>No delegate found</CardTitle>
-          </CardHeader>
-          <CardContent>
-            {tokenHolder?.tokenBalanceRaw ? (
-              <>
-                Delegate your {tokenHolder.tokenBalanceRaw} Realms to yourself
-                to create a delegate profile
-                <Button size="sm" onClick={() => delegateRealms()}>
-                  <UserRoundPlus className="mr-2" /> Delegate to Self
-                </Button>
-              </>
-            ) : (
-              <>
-                <p className="mb-4">
-                  You must have at least one Realm on Starknet to create a
-                  delegate profile. Either bridge a Realm from Etherum or
-                  purchase on the marketplace to get started
-                </p>
-                <Button asChild className="mr-4">
-                  <Link href="/account/assets">Bridge</Link>
-                </Button>
-                <Button asChild>
-                  <Link href="/collection/realms">Marketplace</Link>
-                </Button>
-              </>
-            )}
-          </CardContent>
-        </>
-      )}
-    </>
+                </>
+              ) : (
+                <>
+                  <p className="mb-4">
+                    You must have at least one Realm on Starknet to create a
+                    delegate profile. Either bridge a Realm from Etherum or
+                    purchase on the marketplace to get started
+                  </p>
+                  <Button asChild className="mr-4">
+                    <Link href="/account/assets">Bridge</Link>
+                  </Button>
+                  <Button asChild>
+                    <Link href="/collection/realms">Marketplace</Link>
+                  </Button>
+                </>
+              )}
+            </CardContent>
+          </>
+        )}
+      </Card>
+      {delegate && (<div className="sticky top-[5rem] col-span-2 h-[200px]">
+        <Card className="w-auto">
+          <SignInSIWS />
+        </Card>
+      </div>)}
+    </div>
   );
 };
--- apps/nextjs/src/app/account/profile/page.tsx
+++ apps/nextjs/src/app/account/profile/page.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 
-import { Card, CardContent, CardHeader } from "@realms-world/ui";
-
 import { Profile } from "./Profile";
-import { SignInSIWS } from "./SignInSIWS";
 
 export function generateMetadata(): Metadata {
   return {
@@ -17,16 +14,8 @@ export default function Page() {
     <div>
       <div className="w-full">
         <h4 className="my-4 text-3xl">Delegate Profile</h4>
-        <div className="grid grid-cols-5 gap-x-6">
-          <Card className="col-span-3">
-            <Profile />
-          </Card>
-          <div className="sticky top-[5rem] col-span-2 h-[200px]">
-            <Card className="w-auto">
-              <SignInSIWS />
-            </Card>
-          </div>
-        </div>
+
+        <Profile />
       </div>
     </div>
   );
