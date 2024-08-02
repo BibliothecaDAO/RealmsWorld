1099a4db1b0cbcf5ca913a1b25fde1d236328296
RedBeard <90423049+RedBeardEth@users.noreply.github.com>
2024-08-02 18:10:07 +1000

    Update page.tsx

--- apps/nextjs/src/app/AMMA/page.tsx
+++ apps/nextjs/src/app/AMMA/page.tsx
@@ -1,6 +1,13 @@
 
-export const AMMAPage = () => {
+import { PageLayout } from "../_components/PageLayout";
 
+export const metadata = {
+  title: "AMMA Event",
+  description:
+    "AMMA - Created for adventurers by Bibliotheca DAO",
+};
+
+export default async function Page() {
   return (
     <h1>AMMA</h1>
     )
