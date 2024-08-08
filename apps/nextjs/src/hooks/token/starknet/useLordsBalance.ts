cc022489c7599d0c05369c2f5372beb7f4232e16
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2024-08-09 08:50:32 +1000

    disable bridge remove erros alert unstake

--- apps/nextjs/src/hooks/token/starknet/useLordsBalance.ts
+++ apps/nextjs/src/hooks/token/starknet/useLordsBalance.ts
@@ -1,15 +1,24 @@
+import { useEffect } from "react";
 import { SUPPORTED_L2_CHAIN_ID } from "@/constants/env";
 import { useAccount, useBalance } from "@starknet-react/core";
 
 import { LORDS } from "@realms-world/constants";
 
 export const useLordsBalance = () => {
-  const { address } = useAccount();
-  const { isLoading, data } = useBalance({
+  const { address, isConnected } = useAccount();
+  const { isLoading, data, refetch } = useBalance({
     address,
     watch: true,
     token: LORDS[SUPPORTED_L2_CHAIN_ID]?.address as `0x${string}`,
   });
+
+  useEffect(() => {
+    console.log(address);
+    if (address) {
+      refetch();
+    }
+  }, [isConnected, address, refetch]);
+
   return {
     data,
     isLoading,
