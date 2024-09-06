16c02b3f437c9f5eccb4f17535beacecaea3147b
4eyes52 <transparent1123@gmail.com>
2024-09-06 15:20:56 -0400

    add this back

--- apps/nextjs/src/utils/keystatic.ts
+++ apps/nextjs/src/utils/keystatic.ts
@@ -8,7 +8,7 @@ import keystaticConfig from "../../keystatic.config";
  * Instruct Next.js to include content directory in serverless function 😵
  * Without this Next.js wont be able to do static analysis and "content" directory will not be available in serverless function
  */
-//path.join(process.cwd(), "content");
+path.join(process.cwd(), "content");
 
 export const reader = createReader(process.cwd(), keystaticConfig);
 
