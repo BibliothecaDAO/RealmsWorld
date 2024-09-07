12c42412ab933c352b374a16ae3e7fd82f39121a
4eyes52 <transparent1123@gmail.com>
2024-09-07 14:56:39 -0400

    switch back to github for now

--- apps/nextjs/keystatic.config.ts
+++ apps/nextjs/keystatic.config.ts
@@ -2,7 +2,8 @@ import { collection, config, fields } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: "4eyes52/RealmsWorld"
   },
   ui: {
     brand: { name: "Realms.World" },
