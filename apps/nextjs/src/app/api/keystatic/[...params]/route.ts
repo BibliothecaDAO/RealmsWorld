0e2df3056474843627a8f46e1f7350634e3bf341
4eyes52 <transparent1123@gmail.com>
2024-09-08 07:00:13 -0400

    switch to env

--- apps/nextjs/src/app/api/keystatic/[...params]/route.ts
+++ apps/nextjs/src/app/api/keystatic/[...params]/route.ts
@@ -1,10 +1,11 @@
+import { env } from "@/env";
 import { makeRouteHandler } from "@keystatic/next/route-handler";
 
 import keystaticConfig from "../../../../../keystatic.config";
 
 export const { POST, GET } = makeRouteHandler({
-  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
-  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
-  secret: process.env.KEYSTATIC_SECRET,
-  config: keystaticConfig
+  clientId: env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: env.KEYSTATIC_SECRET,
+  config: keystaticConfig,
 });
