d458401410220ea1146714ef2d1f1bd5b489561d
4eyes52 <transparent1123@gmail.com>
2024-09-07 20:16:53 -0400

    remove this

--- apps/nextjs/src/app/api/keystatic/[...params]/route.ts
+++ apps/nextjs/src/app/api/keystatic/[...params]/route.ts
@@ -1,15 +1,7 @@
 import { makeAPIRouteHandler } from '@keystatic/next/api'
 
 import keyStaticConfig from "../../../../../keystatic.config";
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '20mb',
-    },
-    responseLimit: false, 
-    
-  },
-}
+
 export default makeAPIRouteHandler({
   clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
     clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
