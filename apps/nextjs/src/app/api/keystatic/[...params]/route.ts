891f831e026c71680cb75d4a5ad7ae3e4908c7d2
4eyes52 <transparent1123@gmail.com>
2024-09-08 07:28:39 -0400

    fix:

--- apps/nextjs/src/app/api/keystatic/[...params]/route.ts
+++ apps/nextjs/src/app/api/keystatic/[...params]/route.ts
@@ -3,4 +3,9 @@ import { makeRouteHandler } from "@keystatic/next/route-handler";
 
 import config from "../../../../../keystatic.config";
 
-export const { POST, GET } = makeRouteHandler({ config });
+export const { POST, GET } = makeRouteHandler({
+  clientId: env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: env.KEYSTATIC_SECRET,
+  config,
+});
