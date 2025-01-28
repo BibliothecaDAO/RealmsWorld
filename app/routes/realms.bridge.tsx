45143eb8ed5aa7491aafe38138b164abfa35f247
RedBeardEth <90423049+RedBeardEth@users.noreply.github.com>
2025-01-28 13:04:41 +1100

    add components, bridge, apibara indexers (major)

--- app/routes/realms.bridge.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/realms/bridge')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/realms/bridge"!</div>
-}
